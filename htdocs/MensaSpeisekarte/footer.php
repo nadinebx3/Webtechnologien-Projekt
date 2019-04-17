bc4238cbf1183c5f2aacc9cfd9572921ce050843
nadinebx3 <nadinebx3@hotmail.de>
2019-04-17 13:01:53 +0200

    Add files via upload

---
+++ htdocs/MensaSpeisekarte/footer.php
@@ -0,0 +1,29 @@
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-4">
+      <h3>Addresse</h3>
+      <div class="contact-item">
+        <p>HS Hannover,</p>
+        <p>Ricklinger Stadtweg 120 , 30459 Hannover</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Öffnungszeiten</h3>
+      <div class="contact-item">
+        <p>Montag bis Freitag: 8:00 - 16:00 Uhr </p>
+        <p>Samstag bis Sonntag: Geschlossen</p>
+      </div>
+    </div>
+
+  </div>
+  <div class="container-fluid text-center copyrights">
+    <div class="col-md-8 col-md-offset-2">
+
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
