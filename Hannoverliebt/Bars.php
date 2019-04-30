c50de5d315fd593a9bdd1c3b2a547d4b44a0350a
Firas Shmit <31021322+Firas7@users.noreply.github.com>
2019-04-30 12:51:54 +0200

    Add files via upload
    Neue Bars Seite

--- Hannoverliebt/Bars.php
+++ Hannoverliebt/Bars.php
@@ -12,17 +12,17 @@
 	$plz = $_POST["plz"];
 	$nr = 1;
 	if($plz == ""){
-		$db_result = mysqli_query($db_link,"SELECT `Name`, `Strasse`, `Postleitzahl`, `Stadt` FROM bars")
+		$db_result = mysqli_query($db_link,"SELECT `bar_id`, `name`, `strasse`, `plz`, `ort` FROM bar")
 		or die("Fehler: " . mysqli_error($db_link));
 	}else{
-		$db_result = mysqli_query($db_link,"SELECT `Name`, `Strasse`, `Postleitzahl`, `Stadt` FROM bars WHERE Postleitzahl = $plz")
+		$db_result = mysqli_query($db_link,"SELECT `bar_id`, `name`, `strasse`, `plz`, `ort` FROM bar WHERE plz = $plz")
 		or die("Fehler: " . mysqli_error($db_link));
 	}
 
 	echo ('<h1>' . "Bars in deiner Nähe: " . '</h1> <br>');
 	while($row = mysqli_fetch_array($db_result)){
 
-		echo ('<p>' . $nr .  '- ' . $row ['Name'] . ' ' . $row ['Strasse'] . ' ' . $row ['Postleitzahl'] . ' ' . $row ['Stadt'] . '</p>');
+		echo ('<p>' . $nr .  '- ' . $row ['bar_id'] . ' ' . $row ['name'] . ' ' . $row ['strasse'] . ' ' . $row ['plz'] . ' ' . $row['ort'] . '</p>');
 		$nr++;
 	}
 
